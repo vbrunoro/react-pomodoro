963e5b971d6acc6fe5007c666295e8006850c575
vbrunoro <vfmbrunoro@gmail.com>
2024-04-03 23:41:40 -0300

    Add button component

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,16 @@
+import { Button } from "./components/Button"
+
 export function App() {
 
   return (
-    <h1>Hello World</h1>)
-
+    <>
+      <Button variant="primary" />
+      <Button variant="secondary" />
+      <Button variant="success" />
+      <Button variant="danger" />
+      <Button />
+    </>
+  )
 }
 
 export default App
